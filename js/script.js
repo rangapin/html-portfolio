62f578b35924edcc7918e913d7133a65b66d1a06
Richard Angapin <rangapin@gmx.com>
2019-09-15 16:58:20 +0800

    adding css and pics

--- js/script.js
+++ js/script.js
@@ -7,11 +7,28 @@ $(document).ready(function() {
   });
 
   var typed = new Typed(".typed", {
-    strings: ["Software Engineer.", "Web Developer.", "BSc Student."],
+    strings: ["Software Engineer.", "Web Developer.", "Student."],
     typeSpeed: 70,
     loop: true,
-    startDelay: 1000
+    startDelay: 1000,
     showCursor: false
+  });
+
+  $('.owl-carousel').owlCarousel({
+      loop:true,
+      margin:10,
+      nav:true,
+      responsive:{
+          0:{
+              items:1
+          },
+          600:{
+              items:3
+          },
+          1000:{
+              items:5
+          }
+      }
   });
 
 });
